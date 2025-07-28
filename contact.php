b0a2e8eb43299a636f1e98958abee4d5665d38a5
13Fe <13farsfe@gmail.com>
2025-07-29 00:25:49 +0700

    firs upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+
+<link rel="stylesheet" href="style/contact.css">
+<section class="contact-section">
+  <div class="container">
+    <h2>How can we help?</h2>
+    <p>Send us a message!</p>
+    <form>
+      <input type="text" placeholder="First Name">
+      <input type="text" placeholder="Last Name">
+      <input type="email" placeholder="Email Address">
+      <textarea placeholder="Message"></textarea>
+      <button type="submit">Submit</button>
+    </form>
+    <div class="contact-info">
+      <p>JL. Abdi Negara 11st Floor, Bandarlampung, 35211...</p>
+      <p>movementsocialenvironment@gmail.com</p>
+      <p>(+62) 857265865691</p>
+    </div>
+  </div>
+</section>
